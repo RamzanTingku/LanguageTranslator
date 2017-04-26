cc24d11a7fd80e3dd4742570c712decac8df90cc
Ramzan Ullah <ramzantingku@gmail.com>
2017-04-26 18:49:15 +0600

    added spinner

--- app/src/main/java/com/example/ramzanullah/languagetranslator/MainActivity.java
+++ app/src/main/java/com/example/ramzanullah/languagetranslator/MainActivity.java
@@ -4,21 +4,39 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import com.memetix.mst.language.Language;
 import com.memetix.mst.translate.Translate;
 
 
-public class MainActivity extends AppCompatActivity  {
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+
+    String text;
+   // private Map<String, Language> myText;
+   // private TextView Text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+        Spinner spinner = (Spinner) findViewById(R.id.spinner);
+        ArrayAdapter adapter = ArrayAdapter.createFromResource(this, R.array.Language, R.layout.support_simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+
+
+        /*try {
+            myText = Language.values(Language.fromString(text));
+        } catch (Exception e) {
+            e.printStackTrace();
+       }*/
         ((Button) findViewById(R.id.bTranslate)).setOnClickListener(new View.OnClickListener() {
 
             @Override
@@ -26,10 +44,10 @@ public class MainActivity extends AppCompatActivity  {
                 // TODO Auto-generated method stub
 
 
-
                 class bgStuff extends AsyncTask<Void, Void, Void> {
 
                     String translatedText = "";
+
                     @Override
                     protected Void doInBackground(Void... params) {
                         // TODO Auto-generated method stub
@@ -59,24 +77,35 @@ public class MainActivity extends AppCompatActivity  {
         });
 
 
+    }
 
-}
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+        TextView Text = (TextView) view;
+        text = (String) Text.getText();
+    }
 
 
-    public String translate(String text) throws Exception{
+    @Override
+    public void onNothingSelected (AdapterView < ? > parent){
 
+    }
+
+
+    public String translate (String text) throws Exception {
 
         // Set the Client ID / Client Secret once per JVM. It is set statically and applies to all services
         Translate.setClientId("RAMZAN123ULLAH456"); //Change this
         Translate.setClientSecret("B47r0hFnvF9pqQVdxRPLubqKeDR8L27VV2kkmwtPgsA="); //change
 
 
-        String translatedText = "";
-
-        translatedText = Translate.execute(text, Language.FINNISH);
+        String translatedText = Translate.execute(text,Language.fromString(text));
 
         return translatedText;
-    }
 
+
+    }
 
 }
